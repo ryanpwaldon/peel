3c7d09cb354b3fc27d5d83e5a69f989799c28066
Ryan Waldon <ryanpwaldon@gmail.com>
2022-11-21 21:11:52 +0000

    feat: configure nextauth

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,18 +1,27 @@
-import { type NextAuthOptions } from 'next-auth'
-import DiscordProvider from 'next-auth/providers/discord'
-
 import { prisma } from '@peel/db'
+import { ServerClient } from 'postmark'
+import { type NextAuthOptions } from 'next-auth'
+import EmailProvider from 'next-auth/providers/email'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
+
+const postmark = new ServerClient(process.env.POSTMARK_SECRET as string)
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    EmailProvider({
+      async sendVerificationRequest({ identifier, url }) {
+        const result = await postmark.sendEmail({
+          To: identifier,
+          From: `${process.env.POSTMARK_EMAIL_FROM_NAME} <${process.env.POSTMARK_EMAIL_FROM_ADDRESS}>`,
+          Subject: `Log in to Peel`,
+          TextBody: `Click the following link to log in:\n${url}`,
+        })
+        const failed = result.ErrorCode !== 0
+        if (failed) throw new Error(`Email could not be sent: ${{ errorCode: result.ErrorCode, message: result.Message }}`)
+      },
     }),
-    // ...add more providers here
   ],
   callbacks: {
     session({ session, user }) {
