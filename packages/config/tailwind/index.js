68f5c7b976560b89aa688ff7e88158b48d8f810c
Ryan Waldon <ryanpwaldon@gmail.com>
2022-11-21 21:02:56 +0000

    feat: update tailwind config

--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -1,8 +1,38 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const fontSans = ['var(--inter-font)', ...defaultTheme.fontFamily.sans]
+const fontSymbols = ['var(--symbols-font)', ...fontSans]
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}', './src/_app.tsx'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.neutral,
+      },
+      fontFamily: {
+        sans: fontSans,
+        symbols: fontSymbols,
+      },
+      lineHeight: {
+        inherit: 'inherit',
+      },
+      spacing: {
+        'device-bar-top': '47px',
+        'device-bar-bottom': '26px',
+        'content-bottom': '96px',
+      },
+      borderWidth: {
+        hairline: '0.5px',
+      },
+      fontSize: {
+        '2xs': ['0.625rem', '0.75rem'],
+        '3xs': ['0.5rem', '0.625rem'],
+        '3.5xl': ['2.125rem', '2.375rem'],
+      },
+    },
   },
   plugins: [],
 }
