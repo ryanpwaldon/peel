2da82e82fea8d52b5fa334b61861901307e5a015
Ryan Waldon <ryanpwaldon@gmail.com>
2022-11-21 20:25:46 +0000

    feat: add @peel/native (capacitor)

---
+++ apps/native/capacitor.config.ts
@@ -0,0 +1,15 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'surf.peel',
+  appName: 'Peel',
+  backgroundColor: '#f5f5f5',
+  server: { url: 'https://peel.vercel.app' },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+    },
+  },
+}
+
+export default config
