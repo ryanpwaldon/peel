c5d982bef34bc11d30e31938365cc53bda4ab231
Ryan Waldon <ryanpwaldon@gmail.com>
2022-11-24 00:03:23 +0000

    feat: add stormglass router

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -3,12 +3,14 @@ import { authRouter } from './auth'
 import { userRouter } from './user'
 import { waveRouter } from './wave'
 import { locationRouter } from './location'
+import { stormglassRouter } from './stormglass'
 
 export const appRouter = router({
   auth: authRouter,
   user: userRouter,
   wave: waveRouter,
   location: locationRouter,
+  stormglass: stormglassRouter,
 })
 
 // export type definition of API
