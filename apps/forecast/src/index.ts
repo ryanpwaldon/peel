9c948227fb04a442d17a3785d806fd3e0985bd85
Ryan Waldon <ryanpwaldon@gmail.com>
2022-11-25 10:08:59 +0000

    feat: @peel/forecast

--- apps/forecast/src/index.ts
+++ apps/forecast/src/index.ts
@@ -6,14 +6,16 @@ import { createContextInner } from '@peel/api/src/context'
 const main = async () => {
   const serverCtx = createContextInner({ session: null, isServer: true })
   const serverCaller = appRouter.createCaller(serverCtx)
-  const [forecast] = await serverCaller.forecast.all()
-  if (!forecast) throw new Error('No forecast found.')
-  const keyPoint = forecast.points[0]
-  if (!keyPoint) throw new Error('Forecast has no points.')
-  const start = getTzStartOfDay(keyPoint.timezone, new Date())
-  const end = sub(add(start, { days: 7 }), { seconds: 1 })
-  const weatherEvents = await serverCaller.stormglass.tide.findMany({ start, end, lng: keyPoint.lng, lat: keyPoint.lat })
-  console.log(weatherEvents)
+  const forecasts = await serverCaller.forecast.all()
+  for (const [index, forecast] of forecasts.entries()) {
+    const keyPoint = forecast.points[0]
+    if (!keyPoint) throw new Error('Forecast has no points.')
+    const start = getTzStartOfDay(keyPoint.timezone, new Date())
+    const end = sub(add(start, { days: 14 }), { seconds: 1 })
+    const weatherEvents = await serverCaller.stormglass.weather.findMany({ start, end, lng: keyPoint.lng, lat: keyPoint.lat })
+    const updateWeatherEvents = await serverCaller.forecast.updateWeatherEvents({ id: forecast.id, weatherEvents })
+    console.log(`Updated ${index + 1} of ${forecasts.length} forecasts.`)
+  }
 }
 
 main()
