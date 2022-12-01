960413cb897e0da582f0f7c726b86cbc78f2282f
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-01 14:26:07 +1100

    feat: add suspense to homepage

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,29 +1,47 @@
+import { Suspense } from 'react'
 import { trpc } from '@/utils/trpc'
 import List from '@/components/List/List'
 import Symbol from '@/components/Symbol/Symbol'
 import { Sheet } from '@/components/Sheet/Sheet'
 import Page from '@/components/Scaffolding/Page'
+import Spinner from '@/components/Spinner/Spinner'
 import Header from '@/components/Scaffolding/Header'
 import PageTitle from '@/components/Title/PageTitle'
 
-const profileSheet = <Sheet trigger={<Symbol symbol="account_circle" className="text-[28px] font-medium leading-8 text-blue-600" />}>Olá</Sheet>
-
 export default function Home() {
-  const waves = trpc.wave.findMany.useQuery()
   return (
-    <Page header={<Header right={profileSheet} />}>
+    <Suspense fallback={<Loading />}>
+      <Content />
+    </Suspense>
+  )
+}
+
+const ProfileSheet = () => <Sheet trigger={<Symbol symbol="account_circle" className="text-[28px] font-medium leading-8 text-blue-600" />}>Olá</Sheet>
+
+const Content = () => {
+  const [waves] = trpc.wave.findMany.useSuspenseQuery()
+  return (
+    <Page header={<Header right={<ProfileSheet />} />}>
       <div className="pb-content-bottom">
         <PageTitle title="Home" className="px-5" />
-        {waves.data && (
-          <List
-            className="mt-5"
-            items={waves.data?.map((wave) => ({
-              title: wave.name,
-              path: `/wave/${wave.id}`,
-              subtext: `${wave.point.location.region}, ${wave.point.location.country}`,
-            }))}
-          />
-        )}
+        <List
+          className="mt-5"
+          items={waves.map((wave) => ({
+            title: wave.name,
+            path: `/wave/${wave.id}`,
+            subtext: `${wave.point.location.region}, ${wave.point.location.country}`,
+          }))}
+        />
+      </div>
+    </Page>
+  )
+}
+
+const Loading = () => {
+  return (
+    <Page>
+      <div className="flex h-full w-full items-center justify-center">
+        <Spinner />
       </div>
     </Page>
   )
