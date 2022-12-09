bbaefb58ae751a3d2465564ed7b93aa7697ee2a5
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-10 10:10:51 +1100

    feat: update pages

--- apps/web/src/pages/sessions/create/index.tsx
+++ apps/web/src/pages/sessions/create/index.tsx
@@ -1,5 +1,13 @@
 import Page from '@/components/Scaffolding/Page'
+import PageTitle from '@/components/Title/PageTitle'
+import Header from '@/components/Scaffolding/Header'
 
 export default function CreateSessionPage() {
-  return <Page>Create session</Page>
+  return (
+    <Page header={<Header />}>
+      <div className="pb-content-bottom">
+        <PageTitle title="Create session" className="px-5" />
+      </div>
+    </Page>
+  )
 }
--- apps/web/src/pages/sessions/index.tsx
+++ apps/web/src/pages/sessions/index.tsx
@@ -1,5 +1,13 @@
 import Page from '@/components/Scaffolding/Page'
+import PageTitle from '@/components/Title/PageTitle'
+import Header from '@/components/Scaffolding/Header'
 
 export default function SessionsPage() {
-  return <Page>Sessions</Page>
+  return (
+    <Page header={<Header />}>
+      <div className="pb-content-bottom">
+        <PageTitle title="Sessions" className="px-5" />
+      </div>
+    </Page>
+  )
 }
