9b4975389b5fabb4f4d815744e0894979e6a03a9
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-10 19:57:11 +1100

    refactor: Motion to motion

--- apps/web/src/components/Sheet/Sheet.tsx
+++ apps/web/src/components/Sheet/Sheet.tsx
@@ -1,5 +1,5 @@
 import { createPortal } from 'react-dom'
-import { motion as Motion, type PanInfo, useAnimation } from 'framer-motion'
+import { motion, type PanInfo, useAnimation } from 'framer-motion'
 
 interface BottomSheetProps {
   trigger: React.ReactNode
@@ -25,16 +25,16 @@ export function Sheet({ trigger, children }: BottomSheetProps) {
         {trigger}
       </div>
       {createPortal(
-        <div className="w-full h-full fixed top-0 left-0 pointer-events-none z-10">
-          <Motion.div
+        <div className="pointer-events-none fixed top-0 left-0 z-10 h-full w-full">
+          <motion.div
             initial="hidden"
             animate={animation}
             onClick={controls.close}
-            className="w-full h-full absolute top-0 left-0 bg-black bg-opacity-60 z-0"
+            className="absolute top-0 left-0 z-0 h-full w-full bg-black bg-opacity-60"
             variants={{ visible: { opacity: 1, pointerEvents: 'all' }, hidden: { opacity: 0, pointerEvents: 'none' } }}
           />
-          <div className="w-full h-full relative pt-device-bar-top z-10">
-            <Motion.div
+          <div className="relative z-10 h-full w-full pt-device-bar-top">
+            <motion.div
               drag="y"
               initial="hidden"
               dragElastic={0.1}
@@ -43,11 +43,11 @@ export function Sheet({ trigger, children }: BottomSheetProps) {
               dragConstraints={{ top: 0 }}
               variants={{ visible: { y: 0 }, hidden: { y: '100%' } }}
               transition={{ type: 'spring', duration: 0.48, bounce: 0.0 }}
-              className="w-full h-full bg-gray-100 shadow-lg rounded-t-2xl pointer-events-auto"
+              className="pointer-events-auto h-full w-full rounded-t-2xl bg-gray-100 shadow-lg"
             >
               {children}
               <div className="absolute top-1/2 h-full w-full bg-gray-100" />
-            </Motion.div>
+            </motion.div>
           </div>
         </div>,
         document.querySelector('#app') as Element,
