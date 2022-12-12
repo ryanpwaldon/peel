6532a723bb3402b9e4582aaec01b1d1a8bf45a88
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-12 11:36:05 +1100

    feat: refactor page transition consumer

--- apps/web/src/components/PageTransition/PageTransitionConsumer.tsx
+++ apps/web/src/components/PageTransition/PageTransitionConsumer.tsx
@@ -6,33 +6,38 @@ interface PageProps {
   children: React.ReactNode
 }
 
+interface PageTransitionSettings {
+  initial: AnimationProps['initial']
+  animate: AnimationProps['animate']
+  exit: AnimationProps['exit']
+}
+
 export default function PageTransitionConsumer({ children }: PageProps) {
   const { pageTransition } = useContextOrThrow(PageTransitionContext)
 
-  const initial: Record<PageTransition, AnimationProps['initial']> = {
-    forward: { x: '100%' },
-    back: { x: '-25%', zIndex: 0 },
-    none: {},
-  }
-
-  const animate: Record<PageTransition, AnimationProps['animate']> = {
-    forward: { x: 0 },
-    back: { x: 0 },
-    none: {},
-  }
-
-  const exit: Record<PageTransition, AnimationProps['exit']> = {
-    forward: { x: '-25%' },
-    back: { x: '100%', zIndex: 10 },
-    none: {},
+  const transitions: Record<PageTransition, PageTransitionSettings> = {
+    forward: {
+      initial: { x: '100%' },
+      animate: { x: 0, transition: { ease: [0.23, 0.71, 0.36, 1], duration: 0.448 } },
+      exit: { x: '-25%', transition: { ease: [0.23, 0.71, 0.36, 1], duration: 0.448 } },
+    },
+    back: {
+      initial: { x: '-25%', zIndex: 0 },
+      animate: { x: 0, transition: { ease: [0.23, 0.71, 0.36, 1], duration: 0.448 } },
+      exit: { x: '100%', zIndex: 10, transition: { ease: [0.23, 0.71, 0.36, 1], duration: 0.448 } },
+    },
+    none: {
+      initial: {},
+      animate: {},
+      exit: {},
+    },
   }
 
   return (
     <motion.div
-      initial={initial[pageTransition]}
-      animate={animate[pageTransition]}
-      exit={exit[pageTransition]}
-      transition={{ ease: [0.23, 0.71, 0.36, 1], duration: 0.448 }}
+      initial={transitions[pageTransition].initial}
+      animate={transitions[pageTransition].animate}
+      exit={transitions[pageTransition].exit}
       className="absolute top-0 left-0 h-full w-full border-x-hairline border-gray-200 bg-gray-100"
     >
       {children}
