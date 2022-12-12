d1c13eff4971094c80548b7a322e9b4bdaf583f9
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-12 11:40:17 +1100

    feat: allow back to accept page transition

--- apps/web/src/components/Back/Back.tsx
+++ apps/web/src/components/Back/Back.tsx
@@ -1,17 +1,19 @@
 import Link from '@/components/Link/Link'
 import Symbol from '@/components/Symbol/Symbol'
 import { useContextOrThrow } from '@/utils/useContextOrThrow'
+import { PageTransition } from '@/components/PageTransition/PageTransitionProvider'
 import { PreviousRouteContext } from '@/components/PreviousRoute/PreviousRouteProvider'
 
 interface BackProps {
   children?: React.ReactNode
+  pageTransition?: PageTransition
 }
 
-export default function Back({ children }: BackProps) {
+export default function Back({ children, pageTransition = 'slideBack' }: BackProps) {
   const previousRoute = useContextOrThrow(PreviousRouteContext)
 
   return (
-    <Link href={previousRoute} pageTransition="slideBack">
+    <Link href={previousRoute} pageTransition={pageTransition}>
       {children ?? (
         <div className="flex items-center text-base text-blue-600">
           <Symbol className="font-bold" symbol="arrow_back_ios" />
