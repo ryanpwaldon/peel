781ea7930c7b1b0ee28918837df9c4dba6428a3c
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-12 11:29:13 +1100

    feat: allow back to accept children

--- apps/web/src/components/Back/Back.tsx
+++ apps/web/src/components/Back/Back.tsx
@@ -3,13 +3,21 @@ import Symbol from '@/components/Symbol/Symbol'
 import { useContextOrThrow } from '@/utils/useContextOrThrow'
 import { PreviousRouteContext } from '@/components/PreviousRoute/PreviousRouteProvider'
 
-export default function Back() {
+interface BackProps {
+  children?: React.ReactNode
+}
+
+export default function Back({ children }: BackProps) {
   const previousRoute = useContextOrThrow(PreviousRouteContext)
 
   return (
-    <Link className="flex items-center text-base text-blue-600" href={previousRoute} pageTransition="back">
-      <Symbol className="font-bold" symbol="arrow_back_ios" />
-      <span>Back</span>
+    <Link href={previousRoute} pageTransition="back">
+      {children ?? (
+        <div className="flex items-center text-base text-blue-600">
+          <Symbol className="font-bold" symbol="arrow_back_ios" />
+          <span>Back</span>
+        </div>
+      )}
     </Link>
   )
 }
