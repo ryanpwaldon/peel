fc5abda33bd296b53e6c334e57ec0a28baac6b06
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-21 23:42:37 +1100

    feat: add header titles

--- apps/web/src/pages/sessions/create/index.tsx
+++ apps/web/src/pages/sessions/create/index.tsx
@@ -1,11 +1,12 @@
 import Page from '@/components/Page/Page'
 import PageTitle from '@/components/Title/PageTitle'
+import HeaderTitle from '@/components/Title/HeaderTitle'
 import PageTransitionConsumer from '@/components/Page/PageTransitionConsumer'
 
 export default function CreateSessionPage() {
   return (
     <PageTransitionConsumer>
-      <Page>
+      <Page headerCenter={<HeaderTitle title="Home" />}>
         <div className="pb-content-bottom">
           <PageTitle title="Create session" className="px-5" />
         </div>
