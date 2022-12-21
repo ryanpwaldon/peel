b711e92f6893850563614aa1ffa9e0125092252c
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-21 23:40:54 +1100

    fix: page scrolled state

--- apps/web/src/components/Page/Page.tsx
+++ apps/web/src/components/Page/Page.tsx
@@ -18,7 +18,7 @@ export default function Page({ children, headerLeft, headerCenter, headerRight, 
 
   return (
     <div className="flex h-full w-full flex-col">
-      {showHeader && <Header left={headerLeft} center={headerCenter} right={headerRight} fill={headerFill || pageScrollY !== 0} />}
+      {showHeader && <Header left={headerLeft} center={headerCenter} right={headerRight} fill={headerFill || pageScrollY > 0} />}
       <div className={`flex-1 overflow-auto ${showHeader && 'pt-1'} ${className}`} onScroll={(event) => setPageScrollY(event.currentTarget.scrollTop)}>
         {children}
       </div>
