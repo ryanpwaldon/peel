cabc1a541138a90d5632dce2e8484d804a89a4ab
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-21 21:39:10 +1100

    feat: add ButtonText

---
+++ apps/web/src/components/Button/ButtonText.tsx
@@ -0,0 +1,44 @@
+import ButtonBase from '@/components/Button/ButtonBase'
+
+const themes = {
+  black: {
+    initial: 'bg-gray-800',
+    pressed: 'bg-gray-600',
+  },
+  blue: {
+    initial: 'text-blue-600',
+    pressed: 'text-blue-400',
+  },
+}
+
+interface ButtonTextProps {
+  text: string
+  isDisabled?: boolean
+  type?: 'button' | 'submit'
+  theme?: keyof typeof themes
+  onClick?: () => void
+  className?: string
+}
+
+// prettier-ignore
+export default function ButtonText({
+  onClick,
+  text,
+  type = 'button',
+  theme = 'blue',
+  isDisabled = false,
+  className,
+}: ButtonTextProps) {
+  return (
+    <ButtonBase
+      type={type}
+      onClick={onClick}
+      isDisabled={isDisabled}
+      initialClasses={themes[theme].initial}
+      pressedClasses={themes[theme].pressed}
+      defaultClasses={`font-medium text-sm ${className ?? ''}`}
+    >
+      {text}
+    </ButtonBase>
+  )
+}
