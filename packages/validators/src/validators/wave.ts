3e7c8d7532b9a96e7cc63c10579faef71a79afcd
Ryan Waldon <ryanpwaldon@gmail.com>
2022-12-28 19:50:00 +1100

    feat: update validator

--- packages/validators/src/validators/wave.ts
+++ packages/validators/src/validators/wave.ts
@@ -4,7 +4,7 @@ export const validateWaveName = z
   .string()
   .min(3, 'Wave name must be at least 3 characters.')
   .max(100, 'Wave name must be at most 100 characters.')
-  .regex(/^[a-zA-Z0-9_]+$/, 'Wave name must be alphanumeric.')
+  .regex(/^[a-zA-Z0-9 ]+$/, 'Wave name must be alphanumeric.')
 
 // prettier-ignore
 export const validateWaveRideDirection = z
